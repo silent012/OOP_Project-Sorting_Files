96051d78e55ecc2a97d5614d635921cc5197d1a2
unknown <wojciech.maslany1@gmail.com>
2019-12-31 02:10:25 +0100

    Program now works on users chosen directory

--- temp/FileSorter/MainWindow.xaml.cs
+++ temp/FileSorter/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.IO;
+using System.Windows.Forms;
 
 namespace FileSorter
 {
@@ -10,45 +11,40 @@ namespace FileSorter
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void selectBtn_Click(object sender, RoutedEventArgs e)
+        {
             var currentDate = DateTime.Now;
-
-            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);            
-
-            var filesOnDesktop = Directory.EnumerateFiles(desktopPath);
-
-            // Moving desktop files to specific folders
-            #region
-            foreach (string file in filesOnDesktop)
-            {
-                if (Directory.GetLastAccessTime(file) >= currentDate.AddDays(-7))
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Choose a path where you want your files to be sorted.";
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                string directoryPath = fbd.SelectedPath;                
+                var filesInDirectory = Directory.EnumerateFiles(fbd.SelectedPath);
+                foreach (string file in filesInDirectory)
                 {
-                    Directory.CreateDirectory(@"C:\Users\Wojtek\Desktop\Commonly Used");
-                    label1.Content += System.IO.Path.GetFileName(file) + "\n";
-                    File.Move(file, $"{@"C:\Users\Wojtek\Desktop\Commonly Used\"}{ System.IO.Path.GetFileName(file)}");
-                }
-                else if (Directory.GetLastAccessTime(file) < currentDate.AddDays(-7) && Directory.GetLastAccessTime(file) >= currentDate.AddMonths(-1))
-                {
-                    Directory.CreateDirectory(@"C:\Users\Wojtek\Desktop\Rarely Used");
-                    label1.Content += System.IO.Path.GetFileName(file) + "\n";
-                    File.Move(file, $"{@"C:\Users\Wojtek\Desktop\Rarely Used\"}{ System.IO.Path.GetFileName(file)}");
-                }
-                else if (Directory.GetLastAccessTime(file) < currentDate.AddMonths(-1) && Directory.GetLastAccessTime(file) >= currentDate.AddMonths(-2))
-                {
-                    Directory.CreateDirectory(@"C:\Users\Wojtek\Desktop\Barely Used");
-                    label1.Content += System.IO.Path.GetFileName(file) + "\n";
-                    File.Move(file, $"{@"C:\Users\Wojtek\Desktop\Barely Used\"}{ System.IO.Path.GetFileName(file)}");
-                }
-                else if (Directory.GetLastAccessTime(file) < currentDate.AddMonths(-2))
-                {
-                    Directory.CreateDirectory(@"C:\Users\Wojtek\Desktop\Almost Never Used");
-                    label1.Content += System.IO.Path.GetFileName(file) + "\n";
-                    File.Move(file, $"{@"C:\Users\Wojtek\Desktop\Almost Never Used\"}{ System.IO.Path.GetFileName(file)}");
-                } else
-                {
-                    label1.Content = "Weird error";
+                    if (Directory.GetLastAccessTime(file) >= currentDate.AddDays(-7))
+                    {
+                        Directory.CreateDirectory(directoryPath + @"\Commonly Used");
+                        File.Move(file, $"{directoryPath + @"\Commonly Used\"}{ System.IO.Path.GetFileName(file)}");
+                    }
+                    else if (Directory.GetLastAccessTime(file) < currentDate.AddDays(-7) && Directory.GetLastAccessTime(file) >= currentDate.AddMonths(-1))
+                    {
+                        Directory.CreateDirectory(directoryPath + @"\Rarely Used");
+                        File.Move(file, $"{directoryPath + @"\Rarely Used\"}{ System.IO.Path.GetFileName(file)}");
+                    }
+                    else if (Directory.GetLastAccessTime(file) < currentDate.AddMonths(-1) && Directory.GetLastAccessTime(file) >= currentDate.AddMonths(-2))
+                    {
+                        Directory.CreateDirectory(directoryPath + @"\Barely Used");
+                        File.Move(file, $"{directoryPath + @"\Barely Used\"}{ System.IO.Path.GetFileName(file)}");
+                    }
+                    else if (Directory.GetLastAccessTime(file) < currentDate.AddMonths(-2))
+                    {
+                        Directory.CreateDirectory(directoryPath + @"\Almost Never Used");                       
+                        File.Move(file, $"{directoryPath + @"\Almost Never Used\"}{ System.IO.Path.GetFileName(file)}");
+                    }
                 }
             }
-            #endregion
         }
     }
 }
