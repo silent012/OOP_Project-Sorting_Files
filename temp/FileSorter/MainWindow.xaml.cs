5edbb0660a3271424aafc6fd09e377f9577d7577
unknown <wojciech.maslany1@gmail.com>
2020-01-05 17:11:49 +0100

    Added SortFiles class for code clarity

--- temp/FileSorter/MainWindow.xaml.cs
+++ temp/FileSorter/MainWindow.xaml.cs
@@ -15,36 +15,8 @@ namespace FileSorter
 
         private void selectBtn_Click(object sender, RoutedEventArgs e)
         {
-            var currentDate = DateTime.Now;
-            FolderBrowserDialog fbd = new FolderBrowserDialog();
-            fbd.Description = "Choose a path where you want your files to be sorted.";
-            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
-                string directoryPath = fbd.SelectedPath;                
-                var filesInDirectory = Directory.EnumerateFiles(fbd.SelectedPath);
-                foreach (string file in filesInDirectory)
-                {
-                    if (Directory.GetLastAccessTime(file) >= currentDate.AddDays(-7))
-                    {
-                        Directory.CreateDirectory(directoryPath + @"\Commonly Used");
-                        File.Move(file, $"{directoryPath + @"\Commonly Used\"}{ System.IO.Path.GetFileName(file)}");
-                    }
-                    else if (Directory.GetLastAccessTime(file) < currentDate.AddDays(-7) && Directory.GetLastAccessTime(file) >= currentDate.AddMonths(-1))
-                    {
-                        Directory.CreateDirectory(directoryPath + @"\Rarely Used");
-                        File.Move(file, $"{directoryPath + @"\Rarely Used\"}{ System.IO.Path.GetFileName(file)}");
-                    }
-                    else if (Directory.GetLastAccessTime(file) < currentDate.AddMonths(-1) && Directory.GetLastAccessTime(file) >= currentDate.AddMonths(-2))
-                    {
-                        Directory.CreateDirectory(directoryPath + @"\Barely Used");
-                        File.Move(file, $"{directoryPath + @"\Barely Used\"}{ System.IO.Path.GetFileName(file)}");
-                    }
-                    else if (Directory.GetLastAccessTime(file) < currentDate.AddMonths(-2))
-                    {
-                        Directory.CreateDirectory(directoryPath + @"\Almost Never Used");                       
-                        File.Move(file, $"{directoryPath + @"\Almost Never Used\"}{ System.IO.Path.GetFileName(file)}");
-                    }
-                }
-            }
+            SortFiles sf = new SortFiles();
+            sf.Sort();
         }
     }
 }
---
+++ temp/FileSorter/SortFiles.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FileSorter
+{
+    class SortFiles
+    {      
+
+        public void Sort ()
+        {
+            var currentDate = DateTime.Now;
+            FolderBrowserDialog fbd = new FolderBrowserDialog
+            {
+                Description = "Choose a path where you want your files to be sorted."
+            };
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                string directoryPath = fbd.SelectedPath;
+                var filesInDirectory = Directory.EnumerateFiles(fbd.SelectedPath);
+                foreach (string file in filesInDirectory)
+                {
+                    if (Directory.GetLastAccessTime(file) >= currentDate.AddDays(-7))
+                    {
+                        Directory.CreateDirectory(directoryPath + @"\Commonly Used");
+                        File.Move(file, $"{directoryPath + @"\Commonly Used\"}{ Path.GetFileName(file)}");
+                    }
+                    else if (Directory.GetLastAccessTime(file) < currentDate.AddDays(-7) && Directory.GetLastAccessTime(file) >= currentDate.AddMonths(-1))
+                    {
+                        Directory.CreateDirectory(directoryPath + @"\Rarely Used");
+                        File.Move(file, $"{directoryPath + @"\Rarely Used\"}{ Path.GetFileName(file)}");
+                    }
+                    else if (Directory.GetLastAccessTime(file) < currentDate.AddMonths(-1) && Directory.GetLastAccessTime(file) >= currentDate.AddMonths(-2))
+                    {
+                        Directory.CreateDirectory(directoryPath + @"\Barely Used");
+                        File.Move(file, $"{directoryPath + @"\Barely Used\"}{ Path.GetFileName(file)}");
+                    }
+                    else if (Directory.GetLastAccessTime(file) < currentDate.AddMonths(-2))
+                    {
+                        Directory.CreateDirectory(directoryPath + @"\Almost Never Used");
+                        File.Move(file, $"{directoryPath + @"\Almost Never Used\"}{ Path.GetFileName(file)}");
+                    }
+                }
+                MessageBox.Show("Files successfully sorted :)");
+            }
+        }
+    }
+}
